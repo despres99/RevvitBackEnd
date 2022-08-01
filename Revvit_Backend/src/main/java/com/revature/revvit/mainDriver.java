a4a281f672a14b358248d59ac48c7b429ac0f927
despres99 <ethan.despres.99@gmail.com>
2022-08-01 14:24:03 -0400

    Added Main Driver, POM and .gitignore files

--- Revvit_Backend/src/main/java/com/revature/revvit/mainDriver.java
+++ Revvit_Backend/src/main/java/com/revature/revvit/mainDriver.java
@@ -1,4 +1,12 @@
 package com.revature.revvit;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class mainDriver {
+    public static void main(String[] args) {
+        SpringApplication.run(mainDriver.class, args);
+    }
+
 }
