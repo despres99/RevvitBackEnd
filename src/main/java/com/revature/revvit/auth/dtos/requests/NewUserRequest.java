a988f3c982ea795fbbd05902ba416a375100226b
William Marquardt <william940@revature.net>
2022-08-04 13:34:03 -0500

    added user skeleton... again

--- src/main/java/com/revature/revvit/auth/dtos/requests/NewUserRequest.java
+++ src/main/java/com/revature/revvit/auth/dtos/requests/NewUserRequest.java
@@ -1,4 +1,15 @@
 package com.revature.revvit.auth.dtos.requests;
 
+import lombok.*;
+
+//Data is a lombok annotation that adds @ToString, @EqualsAndHashCode, @Getter on all fields, and @Setter on all non-final fields, and @RequiredArgsConstructor
+@Data
+@NoArgsConstructor
 public class NewUserRequest {
+    private String username;
+    private String password;
+    private final String role = "DEFAULT";
+    private String email;
+    private String bio;
+
 }
