ebb56d6336c4a2bf12aa5a63e53b6b74182504fb
MrNoms <tiddle96@gmail.com>
2022-08-05 14:43:37 -0700

    initial User construction

--- src/main/java/com/revature/revvit/user/User.java
+++ src/main/java/com/revature/revvit/user/User.java
@@ -1,4 +1,28 @@
 package com.revature.revvit.user;
 
+import com.azure.spring.data.cosmos.core.mapping.Container;
+import com.azure.spring.data.cosmos.core.mapping.GeneratedValue;
+import com.revature.revvit.usercontent.UserContentModel;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.*;
+import java.util.List;
+
+@Container(containerName = "user")
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id @GeneratedValue private String id;
+    @NonNull private String username;
+    @NonNull @Size(min = 8) private String password;
+
+    @NonNull @Email private String email;
+    @Value("DEFAULT") private String role;
+    @Min(13) private int age;
+    private String bio;
+    private List<UserContentModel> posts;
 }
